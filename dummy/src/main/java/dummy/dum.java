fb3d44272c92f0a48e3394644866e5ec35307ab9
Asus <Asus@LAPTOP-5CVCD5UV>
2022-09-30 15:17:26 +0530

    up

---
+++ dummy/src/main/java/dummy/dum.java
@@ -0,0 +1,36 @@
+package dummy;
+
+import org.testng.annotations.Test;
+
+public class dum {
+	public class CreateTest {
+
+		@Test(groups = "regression")
+		public void Created() {
+			System.out.println("adfda");
+		}
+
+		@Test(groups = "regression")
+		public void updateddd2() 
+		{
+	      System.out.println("Created");
+	      System.out.println("modified");
+	      System.out.println("inserted");
+
+	}
+
+		@Test(groups = "regression")
+		public void Created2() {
+	      System.out.println("Create");
+	}
+		
+		@Test(groups = "smoke")
+		public void Created3()
+		{
+	      System.out.println("Createdyahh");
+
+	}
+		
+	}
+
+}
